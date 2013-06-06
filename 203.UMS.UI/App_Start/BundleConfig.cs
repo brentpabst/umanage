ef28dafc7cff07774e3205ddcc8252b1baecff1c
Brent Pabst <me@brentpabst.com>
2013-06-06 16:02:39 -0400

    Initial commit

---
+++ 203.UMS.UI/App_Start/BundleConfig.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace _203.UMS.UI
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // Modernizr
+            bundles.Add(new ScriptBundle("~/scripts/modernizr").Include("~/Scripts/modernizr-*"));
+
+            // jQuery
+            bundles.Add(new ScriptBundle("~/scripts/jquery").Include(
+                "~/Scripts/jquery-*"));
+
+            // Bootstrap
+            bundles.Add(new ScriptBundle("~/scripts/bootstrap").Include(
+                "~/Scripts/bootstrap.*"));
+            bundles.Add(new StyleBundle("~/styles/bootstrap").Include(
+                "~/Content/bootstrap.*"));
+
+            // Site
+            var site = new Bundle("~/styles/ums")
+                .Include("~/Content/ums.less");
+            site.Transforms.Add(new LessTransform());
+            site.Transforms.Add(new CssMinify());
+            bundles.Add(site);
+
+        }
+
+        public class LessTransform : IBundleTransform
+        {
+            public void Process(BundleContext context, BundleResponse response)
+            {
+                response.Content = dotless.Core.Less.Parse(response.Content);
+                response.ContentType = "text/css";
+            }
+        }
+    }
+}
---
+++ 203.UMS.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_203.UMS.UI.MvcApplication" Language="C#" %>
