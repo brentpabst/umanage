069f8f64452b11272844958c07eea473239aa98c
Brent Pabst <me@brentpabst.com>
2013-12-18 21:06:25 -0500

    Fixing global

---
+++ 203.UMS.Web.Portal/Global.asax.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace _203.UMS.Web.UI
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
