18efc5de6120f887b6436ab52ecffccc69b55fe1
Brent Pabst <me@brentpabst.com>
2013-12-21 18:47:33 -0500

    Added nav bar

---
+++ 203.UMS.Web.UI/App/viewmodels/my/photo.js
@@ -0,0 +1,7 @@
+﻿define([], function () {
+
+    return {
+
+    };
+});
+
--- 203.UMS.Web.UI/App/viewmodels/shell.js
+++ 203.UMS.Web.UI/App/viewmodels/shell.js
@@ -4,7 +4,11 @@
         router: router,
         activate: function () {
             router.map([
-                { route: '', title: 'Dashboard', moduleId: 'viewmodels/dash', nav: true }
+                { route: '', title: 'Dashboard', moduleId: 'viewmodels/dash', nav: true },
+                { route: 'my/info', title: 'My Information', moduleId: 'viewmodels/my/info', nav: true },
+                { route: 'my/account', title: 'My Account', moduleId: 'viewmodels/my/account', nav: true },
+                { route: 'my/password', title: 'My Password', moduleId: 'viewmodels/my/password', nav: true },
+                { route: 'my/photo', title: 'My Photo', moduleId: 'viewmodels/my/photo', nav: true }
             ]).buildNavigationModel()
               .activate();
         }
