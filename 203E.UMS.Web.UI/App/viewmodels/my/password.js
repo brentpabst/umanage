06016f96f114cae1e13743749c85fb72bc047911
Brent Pabst <me@brentpabst.com>
2014-07-27 23:38:06 -0400

    Updated account and password templates

--- 203E.UMS.Web.UI/App/viewmodels/my/password.js
+++ 203E.UMS.Web.UI/App/viewmodels/my/password.js
@@ -1,5 +1,29 @@
-﻿define([], function () {
-    return {
+﻿define(['plugins/router'], function (router) {
+    var expandAll = function () {
+        $('.info-panel .collapse').collapse('show');
+    },
+    hideAll = function () {
+        $('.info-panel .collapse').collapse('hide');
+    }
 
+    return {
+        expandAll: expandAll,
+        hideAll: hideAll,
+        activate: function () {
+            router.on('router:navigation:composition-complete', function () {
+                $('.info-panel .collapse').on('show.bs.collapse', function (e) {
+                    var header = $(e.target).siblings('.info-panel-header')[0];
+                    var icon = $(header).children('span')[0];
+                    $(icon).removeClass('fa-caret-down').addClass('fa-caret-up');
+                });
+            });
+            router.on('router:navigation:composition-complete', function () {
+                $('.info-panel .collapse').on('hide.bs.collapse', function (e) {
+                    var header = $(e.target).siblings('.info-panel-header')[0];
+                    var icon = $(header).children('span')[0];
+                    $(icon).removeClass('fa-caret-up').addClass('fa-caret-down');
+                });
+            });
+        }
     }
 });
