8796c31ebdb5a3b57838a5ad21a787e61a5826b8
Brent Pabst <me@brentpabst.com>
2014-07-29 08:48:19 -0400

    User api stubs

--- 203E.UMS.Web.UI/Controllers/UserController.cs
+++ 203E.UMS.Web.UI/Controllers/UserController.cs
@@ -1,25 +1,100 @@
-﻿using System.Web.Http;
+﻿using _203E.UMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Web.Http;
 
 namespace E203.UMS.Web.UI.Controllers
 {
     [RoutePrefix("api/users")]
     public class UsersController : ApiController
     {
-        // TODO: GET    /users                          Gets a list of users
-        // TODO: GET    /users/me                       Gets the current user's account
-        // TODO: POST   /users/me                       Updates the current user's account
-        // TODO: GET    /users/{id}                     Gets a user's account
-        // TODO: POST   /users/{id}                     Updates a user's account
-        // TODO: GET    /users/{id}/account/enable      Enables a user's account
-        // TODO: GET    /users/{id}/account/disable     Disables a user's account
-        // TODO: GET    /users/{id}/account/unlock      Unlocks a user's account
-        // TODO: GET    /users/{id}/password/expire     Forces the expiration of a user's password
+
+        /// <summary>
+        /// Gets the users.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="System.NotImplementedException"></exception>
+        [Route(""), HttpGet]
+        public IEnumerable<User> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("me"), HttpGet]
+        public User GetCurrentUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("me"), HttpPost]
+        public User UpdateCurrentUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("me/photo"), HttpGet]
+        public HttpResponseMessage GetCurrentUserPhoto()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("me/photo"), HttpPost]
+        public HttpResponseMessage UpdateCurrentUserPhoto(byte[] photo)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("{id}"), HttpGet]
+        public User GetUser(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("{id}"), HttpPost]
+        public User UpdateUser(Guid id, User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("{id}/photo"), HttpGet]
+        public HttpResponseMessage GetUserPhoto(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("{id}/photo"), HttpPost]
+        public HttpResponseMessage UpdateUserPhoto(Guid id, byte[] photo)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("{id}/account/enable"), HttpPost]
+        public HttpResponseMessage EnableAccount(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("{id}/account/disable"), HttpPost]
+        public HttpResponseMessage DisableAccount(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("{id}/account/unlock"), HttpPost]
+        public HttpResponseMessage UnlockAccount(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Route("{id}/password/expire"), HttpPost]
+        public HttpResponseMessage ExpirePassword(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
         // TODO: GET    /users/{id}/password/code       Generates a password reset code
         // TODO: POST   /users/{id}/password/reset      Changes the user's password with a reset code
         // TODO: POST   /users/{id}/password/change     Changes the user's password
-        // TODO: GET    /users/me/photo                 Gets the current user's photo
-        // TODO: POST   /users/me/photo                 Updates the current user's photo
-        // TODO: GET    /users/{id}/photo               Gets a user's photo
-        // TODO: POST   /users/{id}/photo               Updates a user's photo
     }
 }
