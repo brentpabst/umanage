2e79948ce3eb0de41e2f88e7da07d540396573ff
Brent Pabst <brent.pabst@dudesolutions.com>
2014-11-17 16:48:19 -0500

    Basic shell and bundling

--- uManage/Bootstrapper.cs
+++ uManage/Bootstrapper.cs
@@ -2,6 +2,7 @@
 using Nancy.Bootstrapper;
 using Nancy.Conventions;
 using Nancy.TinyIoc;
+using SquishIt.Framework;
 
 namespace uManage
 {
@@ -15,14 +16,29 @@ namespace uManage
             conventions.StaticContentsConventions.AddDirectory("scripts");
             conventions.StaticContentsConventions.AddDirectory("fonts");
             conventions.StaticContentsConventions.AddDirectory("app");
-
         }
 
         protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
         {
             base.ApplicationStartup(container, pipelines);
-
             Nancy.Security.Csrf.Enable(pipelines);
+
+            pipelines.BeforeRequest += (ctx) =>
+            {
+                ctx.ViewBag.Styles = Bundle.Css().RenderCachedAssetTag("styles");
+                ctx.ViewBag.Scripts = Bundle.JavaScript().RenderNamed("scripts");
+                return null;
+            };
+
+            Bundle.Css()
+                .Add("~/content/bootstrap.css")
+                .Add("~/content/bootstrap-theme.css")
+                .Add("~/content/font-awesome.css")
+                .Add("~/content/site.less")
+                .AsCached("styles", "/content/styles.css");
+
+            Bundle.JavaScript()
+                .AsNamed("scripts", "/content/scripts.js");
         }
     }
 }
--- uManage/app/app.js
+++ uManage/app/app.js
@@ -1,9 +1,15 @@
 ﻿'use strict';
 
 angular.module('umsApp', [
-    'ngRoute'
+    'ngRoute',
+    'ngSanitize'
 ]).config([
     '$routeProvider', function ($routeProvider) {
         $routeProvider.otherwise({ redirectTo: '/home' });
     }
-]);
+]).config([
+    '$logProvider', function ($logProvider) {
+        if ($logProvider.debugEnabled) {
+            $logProvider.debugEnabled(true);
+        }
+}]);
