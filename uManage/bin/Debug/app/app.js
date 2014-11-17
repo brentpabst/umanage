04250486cc7c18cc66489e9ce1c27a91dc7f9e8d
Brent Pabst <me@brentpabst.com>
2014-11-17 00:20:32 -0500

    Adding initial nancy and angular code

---
+++ uManage/bin/Debug/app/app.js
@@ -0,0 +1,9 @@
+﻿'use strict';
+
+angular.module('umsApp', [
+    'ngRoute'
+]).config([
+    '$routeProvider', function ($routeProvider) {
+        $routeProvider.otherwise({ redirectTo: '/home' });
+    }
+]);
