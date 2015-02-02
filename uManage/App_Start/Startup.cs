d0fe0f355608898d401f4321996eedc078f2784a
Brent Pabst <brent.pabst@dudesolutions.com>
2015-02-02 17:19:21 -0500

    New project

---
+++ uManage/App_Start/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+using Owin;
+
+namespace uManage
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var config = new HttpConfiguration();
+            
+            // Wire up the routes
+            config.MapHttpAttributeRoutes();
+
+            // Remove the XML formatter
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
---
+++ uManage/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.Owin.Hosting;
+
+namespace uManage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var appUri = ConfigurationManager.AppSettings["appUri"];
+
+            // Start OWIN
+            using (WebApp.Start<Startup>(appUri))
+            {
+                Console.WriteLine("uManage is running on " + appUri);
+                Console.WriteLine("Press [Enter] to terminate uManage.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
