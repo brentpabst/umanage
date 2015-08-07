d9e9c55f12f53c5b9ac9579d5be7b4e85d7c0afe
Brent Pabst <brent.pabst@dudesolutions.com>
2015-08-07 11:53:08 -0400

    Initial API listing code

---
+++ E203.uManage/Controllers/ApiInfoController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace E203.uManage.Controllers
+{
+    [RoutePrefix("api")]
+    public class ApiInfoController : BaseApiController
+    {
+        [Route("")]
+        [HttpGet]
+        [ResponseType(typeof(List<ApiModel>))]
+        public async Task<IHttpActionResult> GetAllApis()
+        {
+            IApiExplorer apiExplorer = Configuration.Services.GetApiExplorer(); ;
+
+            return await Task.Run(() =>
+            {
+                var apiModel = new ApiModel
+                {
+                    Application = "uManage",
+                    Version = Assembly.GetExecutingAssembly().GetName().Version.ToString(),
+                    Links = new Dictionary<string, string>()
+                };
+
+                foreach (var api in apiExplorer.ApiDescriptions)
+                {
+                    apiModel.Links.Add(api.ActionDescriptor.ActionName, api.RelativePath);
+                }
+
+                return Ok(apiModel);
+            });
+        }
+
+        internal class ApiModel
+        {
+            internal string Application { get; set; }
+            internal string Version { get; set; }
+            internal Dictionary<string, string> Links { get; set; }
+        }
+    }
+}
