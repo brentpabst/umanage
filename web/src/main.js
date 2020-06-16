39d2cdc5ce1d15d030fb85756c6d69925f749c96
Brent Pabst <me@brentpabst.com>
2020-06-16 13:25:25 -0400

    Added uikit

--- web/src/main.js
+++ web/src/main.js
@@ -2,6 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
+import UIkit from "uikit";
+import Icons from "uikit/dist/js/uikit-icons";
+import "@/assets/styles/main.scss";
+
+UIkit.use(Icons);
+window.UIkit = UIkit;
 
 Vue.config.productionTip = false;
 
