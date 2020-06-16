60d64c97c5950dc42411de9867ec41f7dc6e3cae
Brent Pabst <me@brentpabst.com>
2020-06-15 22:33:36 -0400

    Clearing old temp code

--- E203.uManage.Directory/IDirectoryContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.DirectoryServices.AccountManagement;
-
-namespace S203.uManage.Directory
-{
-    public interface IDirectoryContext
-    {
-        PrincipalContext LoadAndConnect();
-    }
-}
