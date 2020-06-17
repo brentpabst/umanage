a9b08cb6e03ff7139b49641443ad39c6b7cdbce2
Brent Pabst <me@brentpabst.com>
2020-06-17 15:55:21 -0400

    Updated eslint issues

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -3,12 +3,23 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: [
+    "plugin:vue/strongly-recommended",
+    "eslint:recommended",
+    "plugin:vue-a11y/base",
+    "plugin:vue-a11y/recommended"
+  ],
   parserOptions: {
     parser: "babel-eslint"
   },
+  plugins: [
+    "vue-a11y"
+  ],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+  },
+  globals: {
+    "UIkit": true
   }
 };
--- web/src/App.vue
+++ web/src/App.vue
@@ -1,5 +1,21 @@
 <template>
   <div id="app">
+    <header>
+      <nav class="uk-navbar-container">
+        <div class="uk-container">
+          <div class="uk-navbar">
+            <div class="uk-navbar-left">
+              <router-link
+                to="/"
+                class="uk-navbar-item uk-logo"
+              >
+                Logo
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
     <router-view />
   </div>
 </template>
