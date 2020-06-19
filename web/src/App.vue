0d5c5d12e188607c7267460627b9b73b4449cd70
Brent Pabst <me@brentpabst.com>
2020-06-19 17:35:13 -0400

    Container fix

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,40 +1,39 @@
 <template>
-  <div
-    id="app"
-    class="uk-container"
-  >
+  <div id="app">
     <header class="uk-margin-medium-bottom">
       <nav class="uk-navbar-container uk-box-shadow-small">
-        <div class="uk-navbar">
-          <div class="uk-navbar-left">
-            <router-link
-              to="/"
-              class="uk-navbar-item uk-logo"
-            >
-              <img
-                src="@/assets/logo.svg"
-                alt="umanage logo"
-                uk-svg
+        <div class="uk-container">
+          <div class="uk-navbar">
+            <div class="uk-navbar-left">
+              <router-link
+                to="/"
+                class="uk-navbar-item uk-logo"
               >
-            </router-link>
-          </div>
-          <div class="uk-navbar-right">
-            <ul class="uk-navbar-nav">
-              <li class="uk-active">
-                <a href="#">My Profile</a>
-              </li>
-              <li><a href="#">Directory</a></li>
-            </ul>
+                <img
+                  src="@/assets/logo.svg"
+                  alt="umanage logo"
+                  uk-svg
+                >
+              </router-link>
+            </div>
+            <div class="uk-navbar-right">
+              <ul class="uk-navbar-nav">
+                <li class="uk-active">
+                  <a href="#">My Profile</a>
+                </li>
+                <li><a href="#">Directory</a></li>
+              </ul>
+            </div>
           </div>
         </div>
       </nav>
     </header>
-    <div>
+    <div class="uk-container">
       <router-view />
     </div>
-    <div class="uk-padding-small uk-text-meta">
+    <footer class="uk-container uk-padding-small uk-text-meta">
       Copyright &copy; {{ new Date().getFullYear() }} <a href="https://www.brentpabst.com">Brenton Pabst</a> provided under the <a href="https://github.com/brentpabst/umanage/blob/master/LICENSE">MIT License</a>
-    </div>
+    </footer>
   </div>
 </template>
 
