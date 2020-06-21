a9cccae882693a86ae13307f867bb5c711fc2e97
Brent Pabst <me@brentpabst.com>
2020-06-21 15:01:06 -0400

    Updated offcanvas

--- web/src/App.vue
+++ web/src/App.vue
@@ -42,7 +42,10 @@
                   </div>
                 </div>
               </div>
-              <div class="uk-navbar-item">
+              <div
+                class="uk-navbar-item"
+                uk-toggle="target: #personal-menu"
+              >
                 <img
                   class="uk-border-circle uk-margin-small-right"
                   style="max-height: 30px;"
@@ -50,6 +53,34 @@
                   alt="user picture"
                 >
                 <span uk-icon="chevron-down" />
+              </div>
+              <div
+                id="personal-menu"
+                uk-offcanvas="flip: true; overlay: true"
+              >
+                <div class="uk-offcanvas-bar">
+                  <div class="user-profile-link">
+                    <router-link to="/me">
+                      <img
+                        class="uk-border-circle uk-margin-small-right"
+                        style="max-height: 30px;"
+                        src="https://via.placeholder.com/175/2196f3/ffffff.webp?text=JD"
+                        alt="user picture"
+                      >
+                      Go to Profile <span uk-icon="arrow-right" />
+                    </router-link>
+                  </div>
+                  <button
+                    class="uk-offcanvas-close uk-close-large"
+                    type="button"
+                    uk-close
+                  />
+                  <ul class="uk-nav uk-nav-default">
+                    <li>
+                      <a href="#">Menu Item 1</a>
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
@@ -69,5 +100,15 @@
 .uk-logo {
   max-width: 170px;
   color: #2196f3;
+}
+
+.uk-offcanvas-bar {
+  padding-top: 80px;
+}
+
+.user-profile-link {
+  position: absolute;
+  top: 20px;
+  left: 20px;
 }
 </style>
